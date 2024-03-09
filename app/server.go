0e3109b2077e76d95ded02970d1e9779887ec745
Darko Ivkov <bVNyDUKE@protonmail.com>
2024-03-09 21:39:53 +0100

    add path handling

--- app/server.go
+++ app/server.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-func handleConnection(conn net.Conn) {
-	fmt.Println("Handling connection", conn.RemoteAddr().String())
-	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
-}
+const (
+	ADDR = "0.0.0.0"
+	PORT = "4221"
+)
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", ADDR+":"+PORT)
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -27,6 +29,53 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn)
+		req, err := getRequest(conn)
+		if err != nil {
+			fmt.Println("Error making request object: ", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Handling connection", conn.RemoteAddr().String())
+		go handleReq(req)
 	}
+}
+
+func handleReq(req request) {
+	if req.Path == "/" {
+		req.sendOk()
+	} else {
+		req.sendNotFound()
+	}
+}
+
+type request struct {
+	Method  string
+	Path    string
+	Version string
+	Conn    net.Conn
+}
+
+func (r *request) sendOk() {
+	fmt.Fprintf(r.Conn, "HTTP/1.1 200 OK\r\n\r\n")
+}
+
+func (r *request) sendNotFound() {
+	fmt.Fprintf(r.Conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+}
+
+func getRequest(conn net.Conn) (request, error) {
+	header, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		return request{}, err
+	}
+	vals := strings.Fields(header)
+
+	r := request{
+		Method:  vals[0],
+		Path:    vals[1],
+		Version: vals[2],
+		Conn:    conn,
+	}
+
+	return r, nil
 }
