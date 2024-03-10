8bdda0ad654f319be5d9d9b14a54d118e784eeea
Darko Ivkov <bVNyDUKE@protonmail.com>
2024-03-10 18:50:52 +0100

    file sending

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -13,19 +14,47 @@ const (
 	PORT = "4221"
 )
 
+var directory string
+
+func init() {
+	flag.StringVar(&directory, "directory", "", "directory to look into for files")
+	flag.Parse()
+}
+
 func main() {
 	s := NewServer(ADDR, PORT)
 
 	s.AddHandler("/", func(res Response, req *Request) {
-		fmt.Fprintf(res, "HTTP/1.1 200 OK\r\n\r\n")
+		res.Ok()
 	})
 	s.AddHandler("/echo", func(res Response, req *Request) {
 		if _, match, found := strings.Cut(req.Path, "/echo/"); found {
-			res.text(match)
+			res.Text(match)
 		}
 	})
 	s.AddHandler("/user-agent", func(res Response, req *Request) {
-		res.text(req.Headers["User-Agent"])
+		res.Text(req.Headers["User-Agent"])
+	})
+	s.AddHandler("/files", func(res Response, req *Request) {
+		if directory == "" {
+			res.NotFound()
+			return
+		}
+
+		_, fileName, found := strings.Cut(req.Path, "/files/")
+		if !found || fileName == "" {
+			res.NotFound()
+			return
+		}
+
+		// puts the whole shebang into memory, think about streaming
+		fileContent, err := os.ReadFile(directory + string(os.PathSeparator) + fileName)
+		if err != nil {
+			res.NotFound()
+			return
+		}
+
+		res.File(fileContent)
 	})
 
 	if err := s.Run(); err != nil {
@@ -89,7 +118,7 @@ func (s *Server) Run() error {
 			fmt.Println("Handling connection", conn.RemoteAddr().String())
 			handler, ok := s.getHandlerFromPath(req.Path)
 			if !ok {
-				res.sendNotFound()
+				res.NotFound()
 				return
 			}
 			handler(res, &req)
@@ -131,7 +160,6 @@ func NewRequest(conn net.Conn) (Request, error) {
 			fmt.Println("Malformed header")
 			continue
 		}
-		fmt.Println("HEADER", headerType, headerValue)
 		r.Headers[headerType] = headerValue
 	}
 
@@ -139,24 +167,66 @@ func NewRequest(conn net.Conn) (Request, error) {
 }
 
 type Response struct {
-	conn net.Conn
+	conn          net.Conn
+	status        string
+	contentType   string
+	contentLength int
+	body          []byte
 }
 
 func NewResponse(conn net.Conn) Response {
 	return Response{
-		conn: conn,
+		status: "200 OK",
+		conn:   conn,
 	}
 }
 
-func (r *Response) sendNotFound() {
-	fmt.Fprintf(r.conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+func (r *Response) Ok() {
+	r.status = "200 OK"
+	r.Write()
+}
+
+func (r *Response) NotFound() {
+	r.status = "404 Not Found"
+	r.Write()
+}
+
+func (r *Response) Text(content string) {
+	r.contentType = "text/plain"
+	r.contentLength = len(content)
+	r.body = []byte(content)
+	r.Write()
+}
+
+func (r *Response) File(fileContent []byte) {
+	r.contentType = "application/octet-stream"
+	r.contentLength = len(fileContent)
+	r.body = fileContent
+	r.Write()
+}
+
+func (r Response) Write() {
+	var resBuilder strings.Builder
+
+	resBuilder.WriteString(fmt.Sprintf("HTTP/1.1 %s\r\n", r.status))
+	if r.contentType != "" {
+		resBuilder.WriteString(fmt.Sprintf("Content-Type: %s\r\n", r.contentType))
+	}
+
+	if r.contentLength != 0 {
+		resBuilder.WriteString(fmt.Sprintf("Content-Length: %v\r\n", r.contentLength))
+	}
+
+	resBuilder.WriteString("\r\n")
+
+	if len(r.body) != 0 {
+		resBuilder.WriteString(string(r.body))
+	}
+
+	_, err := fmt.Fprint(r.conn, resBuilder.String())
 	r.conn.Close()
-}
-
-func (r *Response) text(content string) {
-	_, _ = r.Write([]byte(content))
-}
-
-func (r Response) Write(content []byte) (int, error) {
-	return fmt.Fprintf(r.conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%s\r\n", len(content), content)
+	if err != nil {
+		fmt.Println("Error sending response")
+		os.Exit(1)
+	}
 }
